51fb7489dc9d19d569b56263fca0682cdcb49734
PedroGonzalezMarcos <not2pedro@gmail.com>
2023-06-14 18:48:19 +0200

    v2

--- src/Controller/CaptainListController.php
+++ src/Controller/CaptainListController.php
@@ -12,7 +12,7 @@ class CaptainListController extends AbstractController
     public function index(): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => 'Esta es la lista de capitanes:',
             'path' => 'src/Controller/CaptainListController.php',
         ]);
     }
